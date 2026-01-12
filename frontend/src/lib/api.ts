7e27ea8689b0de8bd1c4abc0d16ee0d942693636
Muhammad-Saad-Ahmed <msaad.tcs@gmail.com>
2026-01-12 17:09:45 +0500

    New Add

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/v1';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://new-hackathon-2-reusable-intelligen-peach.vercel.app/api/v1';
 
 const getAuthToken = () => {
   if (typeof window === 'undefined') {
